7f6b2330df16f09a733cc4b448189390b807677a
Miro <miropaxjones@gmail.com>
2025-01-01 10:32:48 +1100

    Update script.js

--- script.js
+++ script.js
@@ -12,6 +12,9 @@ logos.forEach(element => {
     } else {
       // create proxy logo with flag
       var flag = countries.filter(e => e.cc == element.cc);
+      var imgEl = document.createElement("img");
+      imgEl.src = `data:image/svg+xml,<svg viewBox="0 0 100 100"><text x="30" y="30">${flag} - ${element.name}</text></svg>`;
+      logoCont.appendChild(imgEl);
     }
   }
 });
