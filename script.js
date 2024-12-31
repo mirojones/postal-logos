d075d8d64111cb1a8a77644a06e876c84ca80047
Miro <miropaxjones@gmail.com>
2025-01-01 10:09:41 +1100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
-logos.forEach(element => {
-  if (element.uses == false) {
-    // create image el
-  }
+const logoCont = document.querySelector("main");
 
+logos.forEach(element => {
+  if (element.is_logo = true) {
+    if (element.uses == false) {
+      var imgEl = document.createElement("image");
+      logoCont.appendChild(imgEl);
+    }
+  } else {
+    if (element.uses == true) {
+      // Add attributes to used post
+    } else {
+      // create proxy logo with flag
+    }
+  }
 });
