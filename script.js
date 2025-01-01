ff37720be2d0596a84ecd06f8299531c0e241619
Miro <miropaxjones@gmail.com>
2025-01-01 17:09:30 +1100

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,7 @@
 const logoCont = document.querySelector("main");
 
 logos.forEach(element => {
-  alert(element["cc"]);
-  if (element["no_logo"] == false) {
-    if (element["uses"] == false) {
-      var imgEl = document.createElement("img");
-      imgEl.src = `l/${element.cc}.png`;
-      logoCont.appendChild(imgEl);
-    }
-  } else {
+  if (!!element["no_logo"]) {
     if (element.uses == true) {
       // Add attributes to used post
     } else {
@@ -16,6 +9,12 @@ logos.forEach(element => {
       var flag = countries.filter(e => e.cc == element["cc"]);
       var imgEl = document.createElement("img");
       imgEl.src = `data:image/svg+xml,<svg viewBox="0 0 100 100"><text x="30" y="30">${flag} - ${element.name}</text></svg>`;
+      logoCont.appendChild(imgEl);
+    }
+  } else {
+    if (element["uses"] == false) {
+      var imgEl = document.createElement("img");
+      imgEl.src = `l/${element.cc}.png`;
       logoCont.appendChild(imgEl);
     }
   }
