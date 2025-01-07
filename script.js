c57e45c867be4471058725cb8ef69a3b2fde7e05
Miro <miropaxjones@gmail.com>
2025-01-08 09:36:51 +1100

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
-const mainEl = document.querySelector("main");
+const gallery = document.querySelector("main");
 
 logos.forEach(element => {
     var fig = document.createElement("figure"),
       img = document.createElement("img"),
       figCap = document.createElement("figcaption");
     img.src = `l/${element["cc"]}.png`;
+    figCap.textContent = `${element["name"]}, ${element["cc"]}`;
     fig.appendChild(img);
     fig.appendChild(figCap);
     fig.id = element["cc"];
-    mainEl.appendChild(fig);
+    gallery.appendChild(fig);
 });
