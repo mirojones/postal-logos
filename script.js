03b002768084a3f13dda97a0ce324a770a0aa867
Miro <miropaxjones@gmail.com>
2025-01-08 09:24:22 +1100

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,24 @@
 const mainEl = document.querySelector("main");
 
 logos.forEach(element => {
-  var fig = document.createElement("figure"),
-    img = document.createElement("img"),
-    figCap = document.createElement("figcaption");
-  img.src = `l/${element["cc"]}.png`;
-  fig.appendChild(img);
-  fig.appendChild(figCap);
-  mainEl.appendChild(fig);
+  if (element["name"] == true) {
+    var fig = document.createElement("figure"),
+      img = document.createElement("img"),
+      figCap = document.createElement("figcaption");
+    img.src = `l/${element["cc"]}.png`;
+    fig.appendChild(img);
+    fig.appendChild(figCap);
+    mainEl.appendChild(fig);
+  } else if (element["uses"] == true) {
+    // add classes to used post
+  } else if (element["uses"] == false && element["name"] == false) {
+    // proxy logo
+    var fig = document.createElement("figure"),
+      img = document.createElement("img"),
+      figCap = document.createElement("figcaption");
+    img.src = `data:text/svg,<svg viewBox="0 0 100 100"><text x="10" y="10">[no postal service] - ${element["cc"]}</text></svg>`;
+    fig.appendChild(img);
+    fig.appendChild(figCap);
+    mainEl.appendChild(fig);
+  }
 });
